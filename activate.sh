4cd260a1613eef43282403176142887fc0c4d8fd
David Prihoda <david.prihoda@gmail.com>
2021-08-29 09:31:07 +0000

    Add mailchimp newsletter id

--- activate.sh
+++ activate.sh
@@ -9,3 +9,4 @@ export OASIS_DB_PATH=/opt/biophi/OASis_9mers_v1.db
 export MAX_CONTENT_LENGTH=262144 # 256kb max upload file size
 export MAX_INPUTS=100 # maximum number of input antibodies
 export STATS_DB_PATH=/home/biophi/run/stats.db
+export MAILCHIMP_NEWSLETTER=c7bcd0367a4cdbbfe2ef413ff/c5b4d513538dcdb730f72a9db
